efa1593915e24b42d0c436a1454fdd4ecb735efe
Samantha Felix <166247516+SamyFelix@users.noreply.github.com>
2024-10-22 11:14:01 -0300

    baixando

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O javaScript é uma liguagem de programação'
+)
+criaCartao(
+    'geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa uma função'
+)
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 6 x 8',
+    'O resultado é 48'
+)
