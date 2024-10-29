ed4a72458d744458e6fb399d322aac195705b46d
Samantha Felix <166247516+SamyFelix@users.noreply.github.com>
2024-10-29 11:30:42 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,27 +1,27 @@
 criaCartao(
-    'Progamação',
+    'Pergunta',
     'De quem é a famosa frase “Penso, logo existo”?',
     'Descartes: “Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
 )
 
 criaCartao(
-    'Geografia',
+    'Pergunta',
     'Quais o menor e o maior país do mundo?',
     'Vaticano e Rússia: O Vaticano, sede da igreja católica, tem apenas 44 hectares (0,44 km2). A Rússia, localizada em dois continentes (Ásia e Europa), tem 17 milhões de km2.'
 )
 criaCartao(
-    'Progamação',
+    'Pergunta',
     'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
     'João Goulart: João Belchior Marques Goulart (1919-1976) foi o 24º presidente do Brasil, cujo período de governo compreende os anos de 1961 a 1964.'
 
 )
 criaCartao(
-    'Matemática',
+    'Pergunta',
     'Quantas casas decimais tem o número pi?',
     'Infinitas: Ao longo dos tempos, vários estudiosos têm se dedicado a calcular o número pi e já chegaram ao número de 62,8 trilhões de casas decimais.'
 )
 criaCartao(
-    'Matemática',
+    'Pergunta',
     'Quais os principais autores do Barroco no Brasil?',
     'Gregório de Matos, Bento Teixeira e Manuel Botelho de Oliveira.'
 )
